4e489452ad2197eb9bc1eb2b0f08dc6c37908328
bradleyplater <bradley.plater1@gmail.com>
2025-04-26 11:00:57 +0100

    Added playerId validation

--- helpers/validation-helper.ts
+++ helpers/validation-helper.ts
@@ -1,9 +1,14 @@
 const nameRegex = /^[A-Za-z'-]{2,30}$/;
+const playerIdRegex = /^PLR\d{6}$/;
 
 export const isValidName = (name: string): boolean => {
     if (name === undefined) return false;
 
     return isValidString(nameRegex, name);
+};
+
+export const isValidPlayerId = (playerId: string): boolean => {
+    return isValidString(playerIdRegex, playerId);
 };
 
 const isValidString = (regex: RegExp, value: string): boolean => {
