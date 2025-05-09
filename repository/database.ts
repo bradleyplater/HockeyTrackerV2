8395a28d3796d931c179a75ab21cffb529c37e7c
bradleyplater <bradley.plater1@gmail.com>
2025-05-09 14:23:38 +0100

    Added team to database

--- repository/database.ts
+++ repository/database.ts
@@ -3,11 +3,13 @@ import { Collection, Db, MongoClient } from 'mongodb';
 import { IApiKey } from './apiKey.repository';
 import { ISeason } from './seasons.repository';
 import { IPlayer } from './player.repository';
+import { ITeam } from './team.repository';
 
 export interface IHockeyTrackerCollections {
     seasons?: Collection<ISeason>;
     apiKeys?: Collection<IApiKey>;
     player?: Collection<IPlayer>;
+    team?: Collection<ITeam>;
 }
 
 export const collections: IHockeyTrackerCollections = {};
@@ -29,10 +31,12 @@ export async function connectToDatabase() {
         db.collection<ISeason>('Seasons');
     const playerCollection: Collection<IPlayer> =
         db.collection<IPlayer>('Player');
+    const teamCollection: Collection<ITeam> = db.collection<ITeam>('Team');
 
     collections.seasons = seasonsCollection;
     collections.apiKeys = apiKeyCollection;
     collections.player = playerCollection;
+    collections.team = teamCollection;
 
     console.log(
         `Successfully connected to database: ${db.databaseName} and collections: `
