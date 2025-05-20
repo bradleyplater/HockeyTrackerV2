d0567cd12bccaa1d92fb8c5d44fccfbbae225a04
bradleyplater <bradley.plater1@gmail.com>
2025-05-20 22:31:21 +0100

    Added new service to add player to team, added some validation and added comments on next steps for the service

--- services/team.service.ts
+++ services/team.service.ts
@@ -1,5 +1,11 @@
+import { PlayerErrors, TeamErrors } from '../helpers/error-helper';
 import { generateIdWithPrefix } from '../helpers/id-helper';
-import { InsertTeamToDatabase } from '../repository/team.repository';
+import { GetPlayerByIdFromDatabase } from '../repository/player.repository';
+import {
+    GetTeamByIdFromDatabase,
+    InsertTeamToDatabase,
+    ITeam,
+} from '../repository/team.repository';
 
 export const addTeamToDatabase = async (name: string) => {
     const newTeam = {
@@ -13,4 +19,54 @@ export const addTeamToDatabase = async (name: string) => {
     } catch (error) {
         throw error;
     }
+};
+
+/**
+ * Adds a given player to a given team
+ * @param teamId The id of the team to add the player to
+ * @param playerId The id of the player to add to the team
+ * @param playerNumber The number of the player to add to the team (Must be unique for each team)
+ * @returns The updated team
+
+ */
+export const addPlayerToTeam = async (
+    teamId: string,
+    playerId: string,
+    playerNumber: number
+): Promise<ITeam> => {
+    // TODO: Find team and player, validate they exist else return an error
+    const team = await GetTeamByIdFromDatabase(teamId);
+
+    if (!team) {
+        throw TeamErrors.TEAM_NOT_FOUND;
+    }
+
+    const player = await GetPlayerByIdFromDatabase(playerId);
+
+    if (!player) {
+        throw PlayerErrors.PLAYER_NOT_FOUND;
+    }
+
+    const playersAlreadyOnTeam = team.players.some(
+        (teamPlayer) => teamPlayer.playerId === playerId
+    );
+
+    if (playersAlreadyOnTeam) {
+        throw TeamErrors.PLAYER_ALREADY_ON_TEAM;
+    }
+
+    // TODO: Validate the player number is unique for the team
+    const numberAlreadyInUse = team.players.some(
+        (player) => player.number === playerNumber
+    );
+
+    if (numberAlreadyInUse) {
+        throw TeamErrors.PLAYER_NUMBER_IN_USE;
+    }
+
+    // TODO: Update the team with the new player and their number
+    // TODO: Update the polayer with the team id and number
+    // TODO: Return the updated team
+
+    return {} as ITeam;
 };
