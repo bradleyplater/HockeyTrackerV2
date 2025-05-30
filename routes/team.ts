e4ddb6c553bd68f35c6158e9986a900f678dfcc9
bradleyplater <bradley.plater1@gmail.com>
2025-05-30 09:37:23 +0100

    Added route for endpoint

--- routes/team.ts
+++ routes/team.ts
@@ -1,14 +1,11 @@
 import express from 'express';
-import {
-    isValidName,
-    isValidPlayerId,
-    isValidTeamName,
-} from '../helpers/validation-helper';
+import { isValidPlayerId, isValidTeamName } from '../helpers/validation-helper';
 import { ApiKeyValidation } from '../middleware/authentication';
 import { StatusCodes } from 'http-status-codes';
 import * as logger from '../helpers/logger';
-import { addTeamToDatabase } from '../services/team.service';
+import { addPlayerToTeam, addTeamToDatabase } from '../services/team.service';
 import { GetAllTeamsFromDatabase } from '../repository/team.repository';
+import { HockeyTrackerError } from '../helpers/error-helper';
 const router = express.Router();
 
 router.use(ApiKeyValidation);
@@ -58,31 +55,41 @@ router.get('/', async (req, res) => {
     }
 });
 
-// router.patch('/addplayer/:teamid', async (req, res) => {
-//     try {
-//         logger.LogRouteStarted('Add Player to Team');
+// TODO: Integration tests need writing for this method, Catch is currently not working.
+router.patch('/addplayer/:teamid', async (req, res) => {
+    try {
+        logger.LogRouteStarted('Add Player to Team');
 
-//         // validate body
-//         if (ValidateAddPlayerToTeamBody(req.body)) {
-//             logger.LogBadRequest('Invalid Body');
-//             res.status(StatusCodes.BAD_REQUEST).send();
-//             return;
-//         }
+        // validate body
+        if (!ValidateAddPlayerToTeamBody(req.body)) {
+            logger.LogBadRequest('Invalid Body');
+            res.status(StatusCodes.BAD_REQUEST).send();
+            return;
+        }
 
-//         // call service to add player to the team
+        // call service to add player to the team
+        const team = await addPlayerToTeam(
+            req.params.teamid,
+            req.body.playerId,
+            req.body.number
+        );
 
-//         // return 200
+        logger.LogRouteFinished('Add Player to Team');
+        res.status(StatusCodes.OK).json(team).send();
+    } catch (error) {
+        console.log(error);
+        logger.LogRouteUnsuccessfulFinished(
+            'Get All Teams (Internal Server Error)'
+        );
 
-//         logger.LogRouteFinished('Get All Teams');
-//         res.status(StatusCodes.OK).json().send();
-//     } catch (error) {
-//         console.log(error);
-//         logger.LogRouteUnsuccessfulFinished(
-//             'Get All Teams (Internal Server Error)'
-//         );
-//         res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
-//     }
-// });
+        if (error instanceof HockeyTrackerError) {
+            res.status(error.statusCode).send(error.message);
+            return;
+        }
+
+        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+    }
+});
 
 function ValidatePostTeamBody(team: { name: string }): boolean {
     return isValidTeamName(team.name);
